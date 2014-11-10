6e0efa3f90d2605322cfc4811a5670d4c254ee7a
AlbanMontaigu <alban.montaigu@gmail.com>
2014-11-10 14:44:05 +0100

    Switch to devicemapper and fix swap limit

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,6 +10,7 @@
 #
 # Install Kernel 3.14 from wheezy-backports
 #
+echo " --> Updating Kernel from backport to enable docker install"
 echo "deb http://http.debian.net/debian wheezy-backports main" > /etc/apt/sources.list.d/docker.list
 apt-get update -qq
 apt-get install -q -y --force-yes -t wheezy-backports linux-image-amd64
@@ -17,11 +18,13 @@ apt-get install -q -y --force-yes -t wheezy-backports linux-image-amd64
 #
 # Install Docker using the get.docker.com script
 #
+echo " --> Installing docker"
 curl -sSL https://get.docker.com/ | sh
 
 #
 # Add the docker group if it doesn't already exist.
 #
+echo " --> Updating docker group"
 groupadd docker
 
 #
@@ -32,13 +35,32 @@ groupadd docker
 #
 gpasswd -a ${USER} docker
 
+
+#
+# Solves AUFS layers limitation
+#
+echo " --> Switching from aufs to devicemapper to solve layers limitation"
+
+DOCKER_CONFIGURATION_FILE="/etc/default/docker"
+
+if grep -q devicemapper $DOCKER_CONFIGURATION_FILE; then
+    echo " --> $DOCKER_CONFIGURATION_FILE nothing todo !"
+else
+    sed -i 's/#DOCKER_OPTS=".*"/DOCKER_OPTS="--storage-driver=devicemapper"/' $DOCKER_CONFIGURATION_FILE
+    echo " --> $DOCKER_CONFIGURATION_FILE updated !"
+fi
+
 #
 # Restart the Docker daemon
 #
+echo " --> Restarting docker"
 service docker restart
 
+
 #
-# enable memory and swap cgroup
+# enable memory and swap cgroup to avoid docker complains
+#  @see https://meta.discourse.org/t/fix-for-warning-no-swap-limit-support-error-when-bootstrapping-discourse/16622
 #
-perl -p -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g'  /etc/default/grub
+echo " --> Solving swap limit warning"
+sed -ri 's/GRUB_CMDLINE_LINUX="(.*)"/GRUB_CMDLINE_LINUX="\1 cgroup_enable=memory swapaccount=1"/' /etc/default/grub
 /usr/sbin/update-grub
