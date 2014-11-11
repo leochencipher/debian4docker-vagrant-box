b9af9064396bdefce7e29137a25de8c430fde213
Alban Montaigu <alban.montaigu@gmail.com>
2014-11-11 14:56:08 +0100

    Script to customize shell added

---
+++ scripts/customize-profile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+#--------------------------------------------------------------------------------------
+# CUSTOMIZE PROFILE TO MAKE IT MORE FANCY
+#--------------------------------------------------------------------------------------
+
+PROFILE_FILES=("/home/vagrant/.bashrc" "/home/docker/.bashrc")
+
+#
+# For each profile, do the work
+#
+for PROFILE_FILE in ${PROFILE_FILES[@]}; do
+	echo "Customizing $PROFILE_FILE"
+	echo " --> Enabling color prompt"
+	sed -ri 's/#force_color_prompt=yes/force_color_prompt=yes/' $PROFILE_FILE
+
+	echo " --> Enabling all alias"
+	sed -ri 's/#alias/alias/' $PROFILE_FILE
+done
