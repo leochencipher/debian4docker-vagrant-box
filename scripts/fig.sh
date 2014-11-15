181a54777c2108bfd2347281ff30c52adce265e4
Alban Montaigu <alban.montaigu@gmail.com>
2014-11-15 13:16:05 +0100

    Switch to progress bar for curl

--- scripts/fig.sh
+++ scripts/fig.sh
@@ -5,6 +5,6 @@
 # @see http://www.fig.sh/install.html
 # @https://github.com/AlbanMontaigu/docker-enabled-vagrant/blob/master/debian-jessie/vagrant-provision.sh
 #
-curl -o fig -L https://github.com/docker/fig/releases/download/1.0.0/fig-`uname -s`-`uname -m`
+curl --progress-bar -o fig -L https://github.com/docker/fig/releases/download/1.0.0/fig-`uname -s`-`uname -m`
 chmod a+x fig
 sudo mv fig /usr/local/bin
--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 mkdir -p /home/vagrant/.ssh
-curl -L -k 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -o /home/vagrant/.ssh/authorized_keys
+curl -L -k --progress-bar 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -o /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
