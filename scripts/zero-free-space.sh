cf7043db7dd079d383d637ffeeb066f008a2ae07
Alban Montaigu <alban.montaigu@gmail.com>
2014-11-16 13:06:03 +0100

    /boot need zeroing freespace

--- scripts/zero-free-space.sh
+++ scripts/zero-free-space.sh
@@ -7,6 +7,9 @@ echo "#-------------------------------------------------------------------------
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
+dd if=/dev/zero of=/boot/EMPTY bs=1M
+rm -f /boot/EMPTY
+
 #
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
