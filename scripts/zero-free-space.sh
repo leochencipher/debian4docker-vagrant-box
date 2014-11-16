a23755e88ec2f645ba34aabc91d8e28e9e22b7af
Alban Montaigu <alban.montaigu@gmail.com>
2014-11-16 16:52:35 +0100

    No stop on error wanted here

--- scripts/zero-free-space.sh
+++ scripts/zero-free-space.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 
 echo "#---------------------------------------------------------------------------------------------------"
 echo "# Zeroing out the free space to save space in the final image"
