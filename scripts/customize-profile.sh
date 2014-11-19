725363b806a64a788d7f439d6c0f7520741c570b
AlbanMontaigu <alban.montaigu@gmail.com>
2014-11-19 16:04:14 +0100

    Common docker aliases added

--- scripts/customize-profile.sh
+++ scripts/customize-profile.sh
@@ -5,16 +5,39 @@ echo "-------------------------------------"
 echo " Customizing profiles"
 echo "-------------------------------------"
 
-PROFILE_FILES=("/home/vagrant/.bashrc")
+PROFILES=("/home/vagrant")
 
 #
 # For each profile, do the work
 #
-for PROFILE_FILE in ${PROFILE_FILES[@]}; do
-	echo "Customizing $PROFILE_FILE"
+for PROFILE_FILE in ${PROFILES[@]}; do
+	echo "Customizing $PROFILES"
 	echo " --> Enabling color prompt"
-	sed -ri 's/#force_color_prompt=yes/force_color_prompt=yes/' $PROFILE_FILE
+	sed -ri 's/#force_color_prompt=yes/force_color_prompt=yes/' $PROFILES/.bashrc
 
 	echo " --> Enabling all alias"
-	sed -ri 's/#alias/alias/' $PROFILE_FILE
+	sed -ri 's/#alias/alias/' $PROFILES
+
+	#
+	# Custom docker aliases
+	#
+	#  @see http://www.calazan.com/docker-cleanup-commands/
+	#
+	echo " --> Adding custom docker aliases"
+	cat <<EOT >> $PROFILES/.bash_aliases
+# ~/.bash_aliases
+
+# Kill all running containers.
+alias docker-killa='docker kill $(docker ps -q)'
+
+# Delete all stopped containers.
+alias docker-cleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
+
+# Delete all untagged images.
+alias docker-cleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+
+# Delete all stopped containers and untagged images.
+alias docker-clean='docker-cleanc || true && docker-cleani'
+EOT
+
 done
